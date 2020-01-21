26ebf482700460a33632cebc8adf3a6b69849e43
ZeKai Jia <jiazekai1003@gmail.com>
2020-01-22 01:47:59 +0800

    Demo 2.0.8

--- src/main/java/cn/kevin/stu/manage/mapper/SubjectMapper.java
+++ src/main/java/cn/kevin/stu/manage/mapper/SubjectMapper.java
@@ -2,10 +2,24 @@ package cn.kevin.stu.manage.mapper;
 
 import cn.kevin.stu.manage.entity.Subject;
 
+import java.util.List;
+
 /**
  * @author kevin
  */
 public interface SubjectMapper {
     //添加方法
     void addSubject(Subject subject);
+
+    //删除方法 id
+    void deleteSubjectById(Integer subId);
+
+    //修改方法
+    void updateSubject(Subject subject);
+
+    //单个查找方法 id
+    Subject selectSubject(Integer subId);
+
+    //全体查找方法
+    List<Subject> selectAllSubject();
 }
--- src/main/java/cn/kevin/stu/manage/test/TestSubject.java
+++ src/main/java/cn/kevin/stu/manage/test/TestSubject.java
@@ -1,7 +1,7 @@
 package cn.kevin.stu.manage.test;
 
-import cn.kevin.stu.manage.entity.Student;
-import cn.kevin.stu.manage.mapper.StudentMapper;
+import cn.kevin.stu.manage.entity.Subject;
+import cn.kevin.stu.manage.mapper.SubjectMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -18,13 +18,13 @@ import java.util.List;
 /**
  * @author kevin
  */
-public class TestStudent {
+public class TestSubject {
     //这是个SQL会话工厂对象[表示通过会话发出SQL原生语言]
     private SqlSessionFactory sqlSessionFactory;
     //这个是SQL会话，通过他可以发出SQL语句
     private SqlSession sqlSession;
 
-    private StudentMapper studentMapper;
+    private SubjectMapper subjectMapper;
 
     @Before
     public void init() throws Exception {
@@ -37,59 +37,53 @@ public class TestStudent {
         //通过SqlSessionFactory对象获取一个SqlSession会话
         sqlSession = sqlSessionFactory.openSession();
 
-        studentMapper = sqlSession.getMapper(StudentMapper.class);
+        subjectMapper = sqlSession.getMapper(SubjectMapper.class);
     }
 
     @Test
-    public void testInsertStu() {
-        for (int i=1; i<11; i++) {
-            Student student = new Student();
-            student.setStuId(i);
-            student.setStuAge(10+i);
-            student.setStuBirthday(new Date());
-            student.setStuEmail("84592305" + i + "@qq.com");
-            student.setStuGender(i%2);
-            student.setStuName("贾泽楷" + i);
-            studentMapper.addStudent(student);
+    public void testInsertSub() {
+        for (int i=1; i<6; i++) {
+            Subject subject = new Subject();
+            subject.setSubId(i);
+            subject.setSubName("高数重修" + i);
+            subject.setSubTeacherId(i);
+            subject.setSubCredit(i*2);
+            subjectMapper.addSubject(subject);
         }
         System.out.println("Inserted!");
     }
 
     @Test
-    public void testDeleteStu() {
-        studentMapper.deleteStudentById(1);
+    public void testDeleteSub() {
+        subjectMapper.deleteSubjectById(1);
         System.out.println("Deleted!");
     }
 
-    //TODO 照理说 not exists和 exists意义相反，add里是如果存在就不插入，update里是如果存在就修改.
-    // 但是add可以按照逻辑正常操作而update却会100%修改或插入数据，exists失效了？
     @Test
     public void testUpdateStu() {
-        Student student = new Student();
-        student.setStuId(1);
-        student.setStuAge(99);
-        student.setStuBirthday(new Date());
-        student.setStuEmail("999999999@qq.com");
-        student.setStuGender(3);
-        student.setStuName("9贾9泽9楷9");
-        studentMapper.addStudent(student);
+        Subject subject = new Subject();
+        subject.setSubId(1);
+        subject.setSubName("高数专升本");
+        subject.setSubTeacherId(99);
+        subject.setSubCredit(0);
+        subjectMapper.addSubject(subject);
         System.out.println("Updated!");
     }
 
     @Test
     public void testSelectStuId() {
-        Student student;
-        student = studentMapper.selectStudent(1);
-        System.out.println(student);
+        Subject subject;
+        subject = subjectMapper.selectSubject(1);
+        System.out.println(subject);
         System.out.println("Selected!");
     }
 
     @Test
     public void testSelectAllStu() {
-        List<Student> students;
-        students = studentMapper.selectAllStudent();
-        for (Student stu : students) {
-            System.out.println(stu);
+        List<Subject> subjects;
+        subjects = subjectMapper.selectAllSubject();
+        for (Subject sub : subjects) {
+            System.out.println(sub);
         }
         System.out.println("Selected!");
     }
