dfe4a3444e5b1c48c39feb13a610d0a9c7e884fc
ZeKai Jia <jiazekai1003@gmail.com>
2020-01-22 21:26:52 +0800

    Demo 2.1.1

---
+++ src/main/java/cn/kevin/stu/manage/mapper/ScoreMapper.java
@@ -0,0 +1,33 @@
+package cn.kevin.stu.manage.mapper;
+
+import cn.kevin.stu.manage.entity.Score;
+import cn.kevin.stu.manage.entity.Student;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author kevin
+ */
+public interface ScoreMapper {
+    //添加方法
+    void insert(Score score);
+
+    //删除方法 单科 hashmap
+    void delete(Map<String, Object> map);
+
+    //删除方法 学生
+    void deleteAll(Integer stuId);
+
+    //修改方法 id不可被修改
+    void update(Score score);
+
+    //查找方法 单科 hashmap 精确查找 and
+    List<Score> select(Map<String, Object> map);
+
+    //查找方法 多参数多返回 精确查找 or
+    List<Score> selectByAllInfo(Score score);
+
+    //查找党发 及格
+    List<Score> selectPassScore(Integer subScore);
+}
